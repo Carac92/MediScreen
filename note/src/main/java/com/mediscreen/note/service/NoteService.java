1ae8043fc0875e1fa7a2e3ab9f972a3b5b9571b0
Quentin Caracatzanis <quentinversaille@gmail.com>
2023-05-16 18:01:45 +0200

    added log in note service

--- note/src/main/java/com/mediscreen/note/service/NoteService.java
+++ note/src/main/java/com/mediscreen/note/service/NoteService.java
@@ -2,6 +2,7 @@ package com.mediscreen.note.service;
 
 import com.mediscreen.note.model.Note;
 import com.mediscreen.note.repository.NoteRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -9,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class NoteService {
     private final NoteRepository noteRepository;
 
@@ -17,26 +19,33 @@ public class NoteService {
     }
 
     public void deleteAll() {
+        log.info("Deleting all notes");
         noteRepository.deleteAll();
     }
     public void deleteById(String id) {
+        log.info("Deleting note with id: {}", id);
         noteRepository.deleteById(id);
     }
     public void add(Note note) {
         note.setDate(LocalDateTime.now());
+        log.info("Adding note: {}", note);
         noteRepository.save(note);
     }
     public void update(Note note) {
+        log.info("Updating note: {}", note);
         noteRepository.save(note);
     }
     public Note findById(String id){
+        log.info("Finding note with id: {}", id);
         Optional<Note> note = noteRepository.findById(id);
         return note.orElseGet(Note::new);
     }
     public List<Note> findAll() {
+        log.info("Finding all notes");
         return noteRepository.findAll();
     }
     public List<Note> findAllNotesByPatId(String patId) {
+        log.info("Finding all notes by patId: {}", patId);
         return noteRepository.findByPatId(patId);
     }
 }
